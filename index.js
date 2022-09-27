1c55a3226dc009d583b31208ae13d1811fe55f40
Kant <kant@mail.ru>
2022-09-27 17:27:17 +0300

    Начало проекта

---
+++ index.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+const morgan = require('morgan');
+require('dotenv').config();
+const path = require('path');
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+app.use(morgan('dev'));
+app.use(express.static(path.resolve(__dirname, "public")));
+app.use('/public', express.static(path.join(__dirname, 'public')));
+
+const start = async () => {
+    try {
+        await mongoose.connect(process.env.MONGO_SERVER);
+        console.log("Подключились к базе");
+    } catch (error) {
+        console.log(error);
+    }
+};
+
+app.listen(process.env.PORT, () => {
+    console.log("Подключились к серверу");
+});
+
+start();
