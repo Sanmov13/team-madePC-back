43128c3a4030d91ee04db3d2e4ca0338020e0116
Suliman <sssuliman@bk.ru>
2022-09-28 11:21:58 +0300

    Дополнил controller, model мат.платы

--- controllers/math.controller.js
+++ controllers/math.controller.js
@@ -8,65 +8,51 @@ module.exports.math = {
     res.json(data);
   },
   addMath: async (req, res) => {
-    const {
-      name,
-      type,
-      ram,
-      currentRam,
-      videocard,
-      hardCard,
-      currentHardCard,
-      ssd,
-      currentSsd,
-      processor,
-      frame,
-    } = req.body;
-    const data = await Math.create({
-      name,
-      type,
-      ram,
-      currentRam,
-      videocard,
-      hardCard,
-      currentHardCard,
-      ssd,
-      currentSsd,
-      processor,
-      frame,
-    });
+    const data = await Math.create(
+      ({
+        name,
+        type,
+        socket,
+        image,
+        ram,
+        currentRam,
+        videocard,
+        hardCard,
+        currentHardCard,
+        ssd,
+        currentSsd,
+        processor,
+        frame,
+      } = req.body)
+    );
     const result = await data.populate(
       "ram videocard hardcard ssd processor frame"
     );
     res.json(result);
   },
   updateMath: async (req, res) => {
-    const {
-      name,
-      type,
-      ram,
-      currentRam,
-      videocard,
-      hardCard,
-      currentHardCard,
-      ssd,
-      currentSsd,
-      processor,
-      frame,
-    } = req.body;
-    const data = await Math.findByIdAndUpdate(req.params.id, {
-      name,
-      type,
-      ram,
-      currentRam,
-      videocard,
-      hardCard,
-      currentHardCard,
-      ssd,
-      currentSsd,
-      processor,
-      frame,
-    });
-    res.json(data);
+    const data = await Math.findByIdAndUpdate(
+      req.params.id,
+      ({
+        name,
+        type,
+        socket,
+        image,
+        ram,
+        currentRam,
+        videocard,
+        hardCard,
+        currentHardCard,
+        ssd,
+        currentSsd,
+        processor,
+        frame,
+      } = req.body)
+    );
+    const result = await data.populate(
+      "ram videocard hardcard ssd processor frame"
+    );
+    res.json(result);
   },
   deleteMath: async (req, res) => {
     const data = await Math.findByIdAndDelete(req.params.id);
