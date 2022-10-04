4b5ff6cece1bbfbce7f122091bc5ec59b7607427
Suliman <sssuliman@bk.ru>
2022-10-04 16:11:20 +0300

    Подправил реквест

--- controllers/request.controller.js
+++ controllers/request.controller.js
@@ -1,31 +1,34 @@
-const Request = require('../models/Request.model');
+const Request = require("../models/Request.model");
 
-module.exports.reqcontroller = ({
+module.exports.reqcontroller = {
+  addRequest: async (req, res) => {
+    try {
+      const data = await Request.create(
+        ({ name, number, email, comment, comp } = req.body)
+      );
+      const result = await data.populate("comp");
 
-    addRequest: async (req, res) => {
-        try {
-            const data = await Request.create({ name, number, email, comment } = req.body);
-            res.json(data)
-        } catch (e) {
-            return res.status(404).json(e.toString())
-        }
-    },
-
-    getRequest: async (req, res) => {
-        try {
-            const data = await Request.find()
-            res.json(data)
-        } catch (e) {
-            return res.status(404).json(e.toString())
-        }
-    },
-
-    deleteRequest: async (req, res) => {
-        try {
-            const data = await Request.findByIdAndDelete(req.params.id)
-            res.json(data)
-        } catch (e) {
-            return res.status(404).json(e.toString())
-        }
+      res.json(result);
+    } catch (e) {
+      return res.status(404).json(e.toString());
     }
-})
+  },
+
+  getRequest: async (req, res) => {
+    try {
+      const data = await Request.find().populate("comp");
+      res.json(data);
+    } catch (e) {
+      return res.status(404).json(e.toString());
+    }
+  },
+
+  deleteRequest: async (req, res) => {
+    try {
+      const data = await Request.findByIdAndDelete(req.params.id);
+      res.json(data);
+    } catch (e) {
+      return res.status(404).json(e.toString());
+    }
+  },
+};
