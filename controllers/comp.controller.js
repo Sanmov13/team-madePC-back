d1714613cda81370398fdc1f836412af96e610ce
Suliman <sssuliman@bk.ru>
2022-10-04 12:14:03 +0300

    Сделал автоматический подсчет суммы ПК

--- controllers/comp.controller.js
+++ controllers/comp.controller.js
@@ -1,39 +1,72 @@
-const Comp = require('../models/Comp.model');
+const Comp = require("../models/Comp.model");
 
-module.exports.compController = ({
-    addComp: async (req, res) => {
-        try {
-            const data = await Comp.create({ name, price, image, ram, videocard, hardcard, ssd, processor, corpus, cooler, math, powerunits } = req.body);
-            const result = await data.populate('ram ssd processor corpus cooler powerunits videocard hardCard')
-            res.json(result)
-        } catch (e) {
-            return res.status(404).json(e.toString());
-        }
-    },
+module.exports.compController = {
+  addComp: async (req, res) => {
+    try {
+      const data = await Comp.create(
+        ({
+          name,
+          price,
+          image,
+          ram,
+          videocard,
+          hardcard,
+          ssd,
+          processor,
+          corpus,
+          cooler,
+          math,
+          powerunits,
+        } = req.body)
+      );
+      const result = await data.populate(
+        "ram ssd processor corpus cooler powerunits videocard hardCard"
+      );
 
-    getComp: async (req, res) => {
-        try {
-            const data = await Comp.find().populate('ram ssd processor corpus cooler powerunits videocard hardCard')
-            res.json(data)
-        } catch (e) {
-            return res.status(404).json(e.toString());
-        }
-    },
+      const sum = await Number(
+        result.ram.price +
+          result.videocard.price +
+          result.ssd.price +
+          result.processor.price +
+          result.corpus.price +
+          result.cooler.price
+      );
 
-    updateComp: async (req, res) => {
-        try {
-            const data = await Comp.findByIdAndUpdate(req.params.id, { ...req.body })
-        } catch (e) {
-            return res.status(404).json(e.toString());
-        }
-    },
+      const all = await Comp.updateOne(result, {
+        $set: { price: sum },
+      });
 
-    deleteComp: async (req, res) => {
-        try {
-            const data = await Comp.findByIdAndDelete(req.params.id)
-            res.json(data);
-        } catch (e) {
-            return res.status(404).json(e.toString())
-        }
+      return await res.json(result);
+    } catch (e) {
+      return res.status(404).json(e.toString());
     }
-})
+  },
+
+  getComp: async (req, res) => {
+    try {
+      const data = await Comp.find().populate(
+        "ram ssd processor corpus cooler powerunits videocard hardCard"
+      );
+      res.json(data);
+    } catch (e) {
+      return res.status(404).json(e.toString());
+    }
+  },
+
+  updateComp: async (req, res) => {
+    try {
+      const data = await Comp.findByIdAndUpdate(req.params.id, { ...req.body });
+    } catch (e) {
+      return res.status(404).json(e.toString());
+    }
+  },
+
+  deleteComp: async (req, res) => {
+    try {
+      const data = await Comp.findByIdAndDelete(req.params.id);
+      res.json(data);
+    } catch (e) {
+      return res.status(404).json(e.toString());
+    }
+  },
+};
