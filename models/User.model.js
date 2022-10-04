945efae6a3a253b3acc6ffe6ffaf05e84b39df9b
Kant <kant@mail.ru>
2022-10-04 12:27:12 +0300

    зер

--- models/User.model.js
+++ models/User.model.js
@@ -3,10 +3,10 @@ const mongoose = require('mongoose');
 const userSchema = mongoose.Schema({
     login: String,
     password: String,
-    basket: {
+    basket: [{
         type: mongoose.SchemaTypes.ObjectId,
         ref: 'Comp'
-    }
+    }]
 })
 
 const User = mongoose.model("User", userSchema)
