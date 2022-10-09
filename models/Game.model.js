32bed81cecee01d9602f02d64ea7c9b1f5837fcb
Surkho <asa8964@mail.ru>
2022-10-09 20:02:02 +0300

    Немного изменил бэк

--- models/Game.model.js
+++ models/Game.model.js
@@ -3,8 +3,9 @@ const mongoose = require("mongoose")
 
 const gamesSchema = mongoose.Schema({
     name: String,
-    
     image: String,
+    imageBackground: String,
+    gamePCImg: String,
     comp: [{
             type: mongoose.SchemaTypes.ObjectId,
             ref: "Comp"
